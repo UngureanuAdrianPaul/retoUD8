a5c1ca45e30aa0184b612192bd6053f7e17500ad
UngureanuAdrianPaul <adrianpaul.s371152@cesurformnacion.com>
2024-05-30 12:10:56 +0200

    primer dia de trabajo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
+import CRUD.MiCrud;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Adrian!");
+    public static void main(String[] args) {
+
+        MiCrud miAcceso = new MiCrud("cinema");
+
+        miAcceso.insertRow("cines", new String[] { "direccion", "identificador", "nombre_del_cine" },
+                new Object[] { "Calle Blas Infante", 2, "Lope de Vega" });
+
         
     }
 }
